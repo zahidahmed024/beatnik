13dcaafca351a1590e60aaa110a720ce0ce5a35d
Zahid Ahmed <zahidahmed024@gmail.com>
2022-05-17 17:55:39 +0600

    added extra pie charts

--- src/components/home-screen/PieCharts.js
+++ src/components/home-screen/PieCharts.js
@@ -10,45 +10,60 @@ export default function PieCharts() {
         { value: 30, color: colors.danger }
     ];
     return (
-        <View style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            paddingVertical: ms(20)
-        }}>
-            <PieChart
-                donut
-                radius={60}
-                innerRadius={55}
-                data={pieData}
-                centerLabelComponent={() => {
-                    return (
-                        <View style={{ backgroundColor: colors.secondary, alignItems: 'center', justifyContent: 'center', width: 100, height: 100, borderRadius: 50 }}>
-                            <Text style={{ fontSize: 16, color: colors.white, fontWeight: 'bold' }}>70%</Text>
-                            <Text style={{ fontSize: 16, color: colors.white }}>Completed</Text>
-                        </View>
-                    )
+        <View>
+            <View style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                paddingVertical: ms(20)
+            }}>
+                <PieChart
+                    donut
+                    radius={60}
+                    innerRadius={55}
+                    data={pieData}
+                    centerLabelComponent={() => {
+                        return (
+                            <View style={{ backgroundColor: colors.secondary, alignItems: 'center', justifyContent: 'center', width: 100, height: 100, borderRadius: 50 }}>
+                                <Text style={{ fontSize: 16, color: colors.white, fontWeight: 'bold' }}>70%</Text>
+                                <Text style={{ fontSize: 16, color: colors.white }}>Completed</Text>
+                            </View>
+                        )
 
-                }}
-            />
-            <View>
-                <TextWithColor
-                    color={colors.primary}
-                    text1={"Target"}
-                    text2={"BDT 170,000tk"}
+                    }}
                 />
-                <TextWithColor
-                    color={colors.secondary}
-                    text1={"Achieved"}
-                    text2={"BDT 170,000tk"}
-                />
-                <TextWithColor
-                    color={colors.danger}
-                    text1={"Gap"}
-                    text2={"BDT 170,000tk"}
-                />
+                <View>
+                    <TextWithColor
+                        color={colors.primary}
+                        text1={"Target"}
+                        text2={"BDT 170,000tk"}
+                    />
+                    <TextWithColor
+                        color={colors.secondary}
+                        text1={"Achieved"}
+                        text2={"BDT 170,000tk"}
+                    />
+                    <TextWithColor
+                        color={colors.danger}
+                        text1={"Gap"}
+                        text2={"BDT 170,000tk"}
+                    />
+                </View>
+            </View >
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', padding: ms(10) }}>
+                <SingleChart pieData={pieData} />
+                <SingleChart pieData={pieData} />
+                <SingleChart pieData={pieData} />
             </View>
-        </View >
+            <View style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                padding: ms(10),
+            }}>
+                <TextWithMoney />
+                <TextWithMoney />
+            </View>
+        </View>
     );
 }
 
@@ -85,6 +100,39 @@ function TextWithColor({ color, text1, text2 }) {
                     {text2}
                 </Text>
             </View>
-        </View >
+
+        </View>
+    )
+}
+
+
+function SingleChart({ pieData }) {
+    return (
+        <PieChart
+            donut
+            radius={35}
+            innerRadius={30}
+            data={pieData}
+            centerLabelComponent={() => {
+                return (
+                    <View style={{ backgroundColor: colors.secondary, alignItems: 'center', justifyContent: 'center', width: 50, height: 50, borderRadius: 50 }}>
+                        <Text style={{ fontSize: 10, color: colors.white, fontWeight: 'bold' }}>70%</Text>
+                        <Text style={{ fontSize: 10, color: colors.white }}>Completed</Text>
+                    </View>
+                )
+
+            }}
+        />
+    )
+}
+
+function TextWithMoney() {
+    return (
+        <View style={{
+            paddingVertical: ms(15)
+        }}>
+            <Text style={{ ...fontSizes.h5, fontWeight: 'bold', color: colors.primary }}>Target</Text>
+            <Text style={{ ...fontSizes.h3, fontWeight: 'bold' }}>BDT 170,000</Text>
+        </View>
     )
 }
